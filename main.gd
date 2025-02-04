af6006160a73be3966e9ba83783bbeb3f27718f5
Ian Dodds <ianmitchelldodds@gmail.com>
2025-02-04 14:18:07 -0500

    Variables 2.0

--- main.gd
+++ main.gd
@@ -1,16 +1,39 @@
 extends Node
 
-var health = 100
+# Global variable
+var script_variable = 100
 
-func _input(event) -> void:
-	if event.is_action_pressed("my_action"):
-		health -= 20
-		print(health)
-		
-		if health <= 0:
-			health = 0
-			print ("YOU DIED!")
-		elif health < 50:
-			print("YOU ARE INJURED!")
-		else:
-			print("YOU ARE HEALTHY.")
+# Static variable
+var damage: int = 15 #var damage := 15 (inferred typing)
+@export var health := 100 # variable can be set in the inspector
+
+# Constant
+const GRAVITY = -9.81 # Can not be changed
+
+func _ready():
+	
+	# Local variable
+	var ready_variable = 100
+	
+	# Reassigning variable
+	var godot_is_cool = true
+	var coolness = 9001
+	coolness = true
+	
+	# Casting variables
+	var number = 42
+	var text = "Meaning of life: " + str(number)
+	print(text)
+	
+	var pi = 3.14
+	print(int(pi))
+	
+	# Vector2, Vector3
+	var vec2 = Vector2(0.0, 0.0)
+	var vec3 = Vector3(0.0, 0.0, 0.0)
+	
+	var position = Vector3(3, -10, 5)
+	position.x += 2
+	print(position)
+	
+	pass
