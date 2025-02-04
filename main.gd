776351fcfeff5dfa14d21ad35bcf2653359c52aa
Ian Dodds <ianmitchelldodds@gmail.com>
2025-02-04 10:51:43 -0500

    Variables 1.0

--- main.gd
+++ main.gd
@@ -1,14 +1,8 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$Label.text = "Hello, world!"
-	$Label.modulate = Color.GREEN
+var health = 100
 
 func _input(event) -> void:
 	if event.is_action_pressed("my_action"):
-		$Label.modulate = Color.RED
-	
-	if event.is_action_released("my_action"):
-		$Label.modulate = Color.GREEN
+		health -= 20
+		print(health)
