7d91d660ddc213c5ad4a8f5639235ea44875a380
Ian Dodds <ianmitchelldodds@gmail.com>
2025-02-04 10:46:41 -0500

    input

--- main.gd
+++ main.gd
@@ -5,3 +5,10 @@ extends Node
 func _ready() -> void:
 	$Label.text = "Hello, world!"
 	$Label.modulate = Color.GREEN
+
+func _input(event) -> void:
+	if event.is_action_pressed("my_action"):
+		$Label.modulate = Color.RED
+	
+	if event.is_action_released("my_action"):
+		$Label.modulate = Color.GREEN
