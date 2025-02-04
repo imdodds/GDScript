785e5e5aead510aeb092b323c46a637689c7a734
Ian Dodds <ianmitchelldodds@gmail.com>
2025-02-04 11:14:54 -0500

    If-statements

--- main.gd
+++ main.gd
@@ -6,3 +6,11 @@ func _input(event) -> void:
 	if event.is_action_pressed("my_action"):
 		health -= 20
 		print(health)
+		
+		if health <= 0:
+			health = 0
+			print ("YOU DIED!")
+		elif health < 50:
+			print("YOU ARE INJURED!")
+		else:
+			print("YOU ARE HEALTHY.")
