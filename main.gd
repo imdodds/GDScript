f25b84e7110a8d5c1803d8ada5d6e8c43ee5d555
Ian Dodds <ianmitchelldodds@gmail.com>
2025-02-05 10:11:04 -0500

    Functions

--- main.gd
+++ main.gd
@@ -1,39 +1,17 @@
 extends Node
 
-# Global variable
-var script_variable = 100
+func _input(event):
+	if event.is_action_pressed("my_action"):
+		jump()
 
-# Static variable
-var damage: int = 15 #var damage := 15 (inferred typing)
-@export var health := 100 # variable can be set in the inspector
-
-# Constant
-const GRAVITY = -9.81 # Can not be changed
+func jump():
+	print("JUMP!")
 
 func _ready():
-	
-	# Local variable
-	var ready_variable = 100
-	
-	# Reassigning variable
-	var godot_is_cool = true
-	var coolness = 9001
-	coolness = true
-	
-	# Casting variables
-	var number = 42
-	var text = "Meaning of life: " + str(number)
-	print(text)
-	
-	var pi = 3.14
-	print(int(pi))
-	
-	# Vector2, Vector3
-	var vec2 = Vector2(0.0, 0.0)
-	var vec3 = Vector3(0.0, 0.0, 0.0)
-	
-	var position = Vector3(3, -10, 5)
-	position.x += 2
-	print(position)
-	
-	pass
+	var result = add(3, 5)
+	result = add(result, 10)
+	print(result)
+
+func add(num1: int, num2: int) -> int:
+	var result = num1 + num2
+	return result
