d0ede7edb20e4b91cd393a51f01c5b8818ff42f8
Ian Dodds <ianmitchelldodds@gmail.com>
2025-02-05 10:49:12 -0500

    Arrays

--- main.gd
+++ main.gd
@@ -1,12 +1,13 @@
 extends Node
 
 func _ready():
+	var items: Array[String] = ["Potion", "Feather", "Stolen Harp"]
+	print(items[0])
 	
-	var roll = randf()
-	if roll <= 0.8:
-		print("You found a COMMON item.")
-	else:
-		print("You found a RARE item.")
-
-	var character_height = randi_range(140, 210)
-	print("Your character is " + str(character_height) + " cm tall.")
+	items[1] = "Smelly sock"
+	items[2] = "Staff"
+	print(items)
+	
+	items.remove_at(1)
+	items.append("Overpowered Sword")
+	print(items)
