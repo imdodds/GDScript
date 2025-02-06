ee7fa21ce0bb61e83dcd2966e98150cd1b312210
Ian Dodds <ianmitchelldodds@gmail.com>
2025-02-06 14:12:23 -0500

    Loops

--- main.gd
+++ main.gd
@@ -2,12 +2,25 @@ extends Node
 
 func _ready():
 	var items: Array[String] = ["Potion", "Feather", "Stolen Harp"]
-	print(items[0])
 	
-	items[1] = "Smelly sock"
-	items[2] = "Staff"
-	print(items)
+	# For loops
+	for item in items:
+		if item.length() > 6:
+			print(item)
+
+	for n in 8:
+		print(n)
+
+	# While loops
+	var glass := 0.0
 	
-	items.remove_at(1)
-	items.append("Overpowered Sword")
-	print(items)
+	while glass < 0.5:
+		glass +=randf_range(0.01, 0.2)
+		
+		#if glass > 0.2:
+			#break
+		
+		print(glass)
+	
+	print("The glass is now half full")
+	
