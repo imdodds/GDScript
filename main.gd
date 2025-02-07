e03eca328a808110a343144b309d3a33bf48cca0
Ian Dodds <ianmitchelldodds@gmail.com>
2025-02-07 12:36:45 -0500

    Classes

--- main.gd
+++ main.gd
@@ -1,25 +1,6 @@
 extends Node
 
-signal health_changed(new_health)
-
-var health := 100:
-	set(value):
-		health = clamp(value, 0, 100)
-		health_changed.emit(health)
-
-var chance := 0.2
-var chance_pct: int:
-	get:
-		return chance * 100
-	set(value):
-		chance = float(value) / 100.0
+@export var character_to_kill : Character
 
 func _ready():
-	health = -150
-	
-	print(chance_pct)
-	chance_pct = 40
-	print(chance_pct)
-
-func _on_health_changed(new_health: Variant) -> void:
-	print("Health: " + str(new_health))
+	character_to_kill.die()
