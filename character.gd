e189f884b16c462aaff161a91073e1da0a63aa1f
Ian Dodds <ianmitchelldodds@gmail.com>
2025-02-07 14:24:46 -0500

    Inner classes

--- character.gd
+++ character.gd
@@ -2,9 +2,14 @@ class_name Character
 
 extends Node
 
-@export var profession : String
-@export var health : int
+var chest := Equipment.new()
+var legs := Equipment.new()
 
-func die():
-	health = 0
-	print(profession + " died.")
+func _ready():
+	chest.armor = 20
+	print(chest.armor)
+	print(chest.weight)
+
+class Equipment:
+	var armor := 10
+	var weight := 5
--- main.gd
+++ main.gd
@@ -1,6 +1,4 @@
 extends Node
 
-@export var character_to_kill : Character
-
 func _ready():
-	character_to_kill.die()
+	pass
