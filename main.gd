c239edb292bcd3223ec5a2f7814832581bbd9c07
Ian Dodds <ianmitchelldodds@gmail.com>
2025-02-07 12:25:43 -0500

    Get / set

--- main.gd
+++ main.gd
@@ -1,19 +1,25 @@
 extends Node
 
-signal leveled_up(msg)
+signal health_changed(new_health)
 
-var xp := 0
+var health := 100:
+	set(value):
+		health = clamp(value, 0, 100)
+		health_changed.emit(health)
 
-#func _ready():
-	#leveled_up.connect(_on_leveled_up)
+var chance := 0.2
+var chance_pct: int:
+	get:
+		return chance * 100
+	set(value):
+		chance = float(value) / 100.0
 
-func _on_timer_timeout() -> void:
-	xp += 5
-	print(xp)
-	if xp >= 20:
-		xp = 0
-		leveled_up.emit("GZ!")
+func _ready():
+	health = -150
+	
+	print(chance_pct)
+	chance_pct = 40
+	print(chance_pct)
 
-
-func _on_leveled_up(msg) -> void:
-	print(msg)
+func _on_health_changed(new_health: Variant) -> void:
+	print("Health: " + str(new_health))
